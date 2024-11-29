16ccc5a888dc16622324c8e5112680b44cc6cbd0
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-11-29 15:37:42 +0530

    feat: error handling

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import { config } from "./config/app.config";
 
 // Import models and database
 import db from "./database/database";
+import { errorHandler } from "./middleware/errorHandler";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middleware/asyncHandler";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -20,11 +23,16 @@ app.use(
 );
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json({
-    message: "Health check successful",
-  });
-});
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) =>
+    res.status(HTTPSTATUS.OK).json({
+      message: "Health check successful",
+    })
+  )
+);
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT}`);
