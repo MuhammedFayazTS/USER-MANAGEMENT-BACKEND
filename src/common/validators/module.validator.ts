0db49265402132e4eb65ee0212fed16f7f4e9fa0
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-12-27 11:34:25 +0530

    refactor: modify validators

--- src/common/validators/module.validator.ts
+++ src/common/validators/module.validator.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const stringSchema = z.string().trim().min(6).max(255);
+const stringSchema = z.string().trim().min(1).max(255);
 
 export const moduleSchema = z.object({
   name: stringSchema,
--- src/common/validators/permission.validator.ts
+++ src/common/validators/permission.validator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
-const nameSchema = z.string().trim().min(6).max(255);
-const descriptionSchema = z.string().trim().min(6).max(255);
+const nameSchema = z.string().trim().min(1).max(255);
+const descriptionSchema = z.string().trim().max(255).optional();
 
 export const permissionSchema = z.object({
   name: nameSchema,
