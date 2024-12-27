984bcec55243d0d8ebf169c1db9ff9e3b37eccd5
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-12-27 11:34:25 +0530

    refactor: modify validators

--- src/common/validators/role.validatior.ts
+++ src/common/validators/role.validatior.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
-const nameSchema = z.string().trim().min(6).max(255);
-const descriptionSchema = z.string().trim().min(6).max(255);
+const nameSchema = z.string().trim().min(1, "Name is required").max(255);
+const descriptionSchema = z.string().trim().min(1).max(255).optional();
 
 export const roleSchema = z.object({
   name: nameSchema,
